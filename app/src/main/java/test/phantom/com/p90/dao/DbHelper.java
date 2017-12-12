20fc539215abcb708cd1e357ec2e13fd3c9afd9c
chenjian <chenjian@vaneqi.com>
2017-12-12 18:03:37 +0800

    add res

---
+++ app/src/main/java/test/phantom/com/p90/dao/DbHelper.java
@@ -0,0 +1,41 @@
+package test.phantom.com.p90.dao;
+
+import android.database.sqlite.SQLiteDatabase;
+
+import test.phantom.com.p90.injector.BaseApplication;
+
+public class DbHelper {
+    private DaoMaster.DevOpenHelper mHelper;
+    private SQLiteDatabase db;
+    private DaoMaster mDaoMaster;
+    private DaoSession mDaoSession;
+
+    private DbHelper(){
+        mHelper = new DaoMaster.DevOpenHelper(BaseApplication.getInstance(), "phantom_db", null);
+        db = mHelper.getWritableDatabase();
+        // 注意：该数据库连接属于 DaoMaster，所以多个 Session 指的是相同的数据库连接。
+        mDaoMaster = new DaoMaster(db);
+        mDaoSession = mDaoMaster.newSession();
+    }
+
+    private static DbHelper instance;
+
+    public static DbHelper getInstance(){
+        if(null == instance){
+            synchronized (DbHelper.class){
+                if(null == instance){
+                    instance = new DbHelper();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public DaoSession getmDaoSession() {
+        return mDaoSession;
+    }
+
+    public SQLiteDatabase getDb() {
+        return db;
+    }
+}
---
+++ app/src/main/java/test/phantom/com/p90/ui/main/MainPersenter.kt
@@ -0,0 +1,10 @@
+package test.phantom.com.p90.ui.main
+
+import test.phantom.com.p90.base.BasePresenter
+
+/**
+ * Created by phantom on 2017/12/12.
+ */
+class MainPersenter : BasePresenter {
+
+}
